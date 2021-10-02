7ee1ef7ac79c051ed049e4c96134091c2870cf11
Jan Iselt <jan.iselt@invers.com>
2021-10-02 18:21:46 +0200

    update user controller & service

--- snake-srv/controller/user.controller.ts
+++ snake-srv/controller/user.controller.ts
@@ -1,11 +1,17 @@
-import {controller, httpPost, request, requestBody, requestParam, response} from "inversify-express-utils";
+import {controller, httpPost, request, requestBody, requestParam, response, TYPE} from "inversify-express-utils";
 import {Request, Response} from "express";
 import {User} from "../entities/user.entity";
+import {inject} from "inversify";
+import {TYPES} from "../inversify/inversify-types";
+import {UserService} from "../service/user.service";
 
 @controller('/users')
 export class UserController {
+    public constructor(@inject(TYPES.UserService) private readonly userService: UserService) {}
+
+
     @httpPost('/')
-    private addUser(@request() req: Request, @response() res: Response, @requestBody() body: User) {
-        return 'Hello World';
+    private createUser(@request() req: Request, @response() res: Response, @requestBody() body: User) {
+        return this.userService.createUser(body);
     }
 }
--- snake-srv/service/user.service.ts
+++ snake-srv/service/user.service.ts
@@ -1,6 +1,13 @@
-import {injectable} from "inversify";
+import {inject, injectable} from "inversify";
+import {TYPES} from "../inversify/inversify-types";
+import {Logger} from "../util/logger";
+import {User} from "../entities/user.entity";
 
 @injectable()
 export class UserService {
+    public constructor(@inject(TYPES.Logger) private readonly log: Logger) {}
 
+    public async createUser(user: User): Promise<void> {
+        return Promise.resolve();
+    }
 }
